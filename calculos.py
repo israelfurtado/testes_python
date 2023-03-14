20c7be5b4ead628865f48796698d88e8d896b81c
israelfurtado <israel.furtado@outlook.com.br>
2023-03-13 21:54:01 -0300

    Primeiro commit

---
+++ calculos.py
@@ -0,0 +1,5 @@
+import funcoes  
+
+funcoes.apresenta()
+print(funcoes.soma(3, 4))
+print(funcoes.multiplica(3, 4))
---
+++ funcoes.py
@@ -0,0 +1,9 @@
+def apresenta():
+    print("Olá! Este é um teste de importação de bibliotecas e commit no git!")
+
+def soma(a, b):
+    return a + b
+
+def multiplica(a, b):
+    return a * b
+
